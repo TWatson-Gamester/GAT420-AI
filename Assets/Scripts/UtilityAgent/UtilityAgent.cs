2f32bff08fb3ee4f0c3674035dfaa659bfcc3fec
TWatson-Gamester <twatson@student.neumont.edu>
2022-02-18 11:57:18 -0700

    Needs & Mood UI

--- Assets/Scripts/UtilityAgent/UtilityAgent.cs
+++ Assets/Scripts/UtilityAgent/UtilityAgent.cs
@@ -5,23 +5,31 @@ using UnityEngine;
 public class UtilityAgent : Agent
 {
     Need[] needs;
+    [SerializeField] MeterUI meter;
+    public float happiness
+    {
+        get
+        {
+            float totalMotive = 0;
+            foreach(Need need in needs)
+            {
+                totalMotive += need.motive;
+            }
+            return 1 - (totalMotive / needs.Length);
+        }
+    }
 
     private void Start()
     {
         needs = GetComponentsInChildren<Need>();
+        meter.text.text = "";
     }
 
-    private void OnGUI()
-    {
-        Vector2 screen = Camera.main.WorldToScreenPoint(transform.position);
 
-        GUI.color = Color.black;
-        int offset = 0;
-        foreach (var need in needs)
-        {
-            GUI.Label(new Rect(screen.x + 20, Screen.height - screen.y - offset, 300, 20), need.type.ToString() + ": " + need.value);
-            offset += 20;
-        }
-        //GUI.Label(new Rect(screen.x + 20, Screen.height - screen.y - offset, 300, 20), mood.ToString());
+    private void Update()
+    {
+            animator.SetFloat("speed", movement.velocity.magnitude);
+        meter.slider.value = happiness;
+        meter.worldPosition = transform.position + (Vector3.up * 4);
     }
 }
